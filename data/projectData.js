261fcfab9c0bbc151152c8ed6132759728736758
MarianoFranco <69841893+MarianoFranco@users.noreply.github.com>
2022-06-06 16:31:17 +0200

    adding last project

--- data/projectData.js
+++ data/projectData.js
@@ -1,93 +1,96 @@
 const projectsData = [
-    {
-        title: "Campshop",
-        usedLanguages: [
-            {
-                icon: "ci:html5"
-            },
-            {
-                icon: "ci:css3"
-            },
-            {
-                icon: "ci:javascript"
-            }
-        ],
-        imageSrc: "/images/campshop/1.jpg",
-        smallDesc: "An e-commerce website that has both customer-facing and admin sections. The project was populated by Strapi API and deployed in Heroku",
-        desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
-        projectLink: "https://campshop-sp.netlify.app"
-    },
-    {
-        title: "Technoland Museum",
-        usedLanguages: [
-            {
-                icon: "ci:html5"
-            },
-            {
-                icon: "ci:css3"
-            },
-           
-        ],
-        imageSrc: "/images/technoland/1.jpg",
-        smallDesc: "A science museum website. This was my first semester project, in which I could consolidate my knowledge in Adobe xD, for desing, and using flexBox and CSS grids",
-        desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
-        projectLink: "https://thesciencemuseum.netlify.app"
-    },
-    {
-        title: "Nomada Blog",
-        usedLanguages: [
-            {
-                icon: "ci:html5"
-            },
-            {
-                icon: "ci:css3"
-            },
-            {
-                icon: "ci:javascript"
-            }
-        ],
-        imageSrc: "/images/nomada/1.jpg",
-        smallDesc: "A travel blog. This was my first year project. The design was done with Figma and I create a dynamic web which is connected to an API with vanilla Javascript",
-        desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
-        projectLink: "https://nomadatravelblog.netlify.app"
-    },
-    {
-        title: "Lvl Up Gaming",
-        usedLanguages: [
-            {
-                icon: "ci:html5"
-            },
-            {
-                icon: "ci:css3"
-            },
-            {
-                icon: "ci:javascript"
-            }
-        ],
-        imageSrc: "/images/lvlup/lvlup.jpg",
-        smallDesc: "My first project. It is a gaming website, which I connected to a Rest API in the end of my first school year.",
-        desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
-        projectLink: "https://lvlupgamesite.netlify.app"
-    },
-    {
-        title: "JavaScript frameworks CA",
-        usedLanguages: [
-            {
-                icon: "ci:html5"
-            },
-            {
-                icon: "ci:css3"
-            },
-            {
-                icon: "ci:javascript"
-            }
-        ],
-        imageSrc: "/images/lvlup/lvlup.jpg",
-        smallDesc: "My first project. It is a gaming website, which I connected to a Rest API in the end of my first school year.",
-        desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
-        projectLink: "https://lvlupgamesite.netlify.app"
-    }
-
+	{
+		title: "Campshop",
+		usedLanguages: [
+			{
+				icon: "ci:html5",
+			},
+			{
+				icon: "ci:css3",
+			},
+			{
+				icon: "ci:javascript",
+			},
+		],
+		imageSrc: "/images/campshop/1.jpg",
+		smallDesc:
+			"An e-commerce website that has both customer-facing and admin sections. The project was populated by Strapi API and deployed in Heroku",
+		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+		projectLink: "https://campshop-sp.netlify.app",
+	},
+	{
+		title: "Technoland Museum",
+		usedLanguages: [
+			{
+				icon: "ci:html5",
+			},
+			{
+				icon: "ci:css3",
+			},
+		],
+		imageSrc: "/images/technoland/1.jpg",
+		smallDesc:
+			"A science museum website. This was my first semester project, in which I could consolidate my knowledge in Adobe xD, for desing, and using flexBox and CSS grids",
+		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+		projectLink: "https://thesciencemuseum.netlify.app",
+	},
+	{
+		title: "Nomada Blog",
+		usedLanguages: [
+			{
+				icon: "ci:html5",
+			},
+			{
+				icon: "ci:css3",
+			},
+			{
+				icon: "ci:javascript",
+			},
+		],
+		imageSrc: "/images/nomada/1.jpg",
+		smallDesc:
+			"A travel blog. This was my first year project. The design was done with Figma and I create a dynamic web which is connected to an API with vanilla Javascript",
+		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+		projectLink: "https://nomadatravelblog.netlify.app",
+	},
+	{
+		title: "Lvl Up Gaming",
+		usedLanguages: [
+			{
+				icon: "ci:html5",
+			},
+			{
+				icon: "ci:css3",
+			},
+			{
+				icon: "ci:javascript",
+			},
+		],
+		imageSrc: "/images/lvlup/lvlup.jpg",
+		smallDesc:
+			"My first project. It is a gaming website, which I connected to a Rest API in the end of my first school year.",
+		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+		projectLink: "https://lvlupgamesite.netlify.app",
+	},
+	{
+		title: "Holidaze hotel booking",
+		usedLanguages: [
+			{
+				icon: "ci:html5",
+			},
+			{
+				icon: "ci:css3",
+			},
+			{
+				icon: "ci:javascript",
+			},
+		],
+		imageSrc: "/images/holidaze/holidaze-portrait.jpg",
+		smallDesc:
+			"My first project. It is a gaming website, which I connected to a Rest API in the end of my first school year.",
+		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+		projectLink: "https://lvlupgamesite.netlify.app",
+	},
 ];
 
 export default projectsData;
