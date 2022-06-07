e8e4665a91fedb1f9764d3a7c46b84e5852fa871
MarianoFranco <69841893+MarianoFranco@users.noreply.github.com>
2022-06-07 12:47:41 +0200

    modifing data

--- data/projectData.js
+++ data/projectData.js
@@ -15,7 +15,7 @@ const projectsData = [
 		imageSrc: "/images/lvlup/lvlup.jpg",
 		smallDesc:
 			"My first project. It is a gaming website, which I connected to a Rest API in the end of my first school year.",
-		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+
 		projectLink: "https://lvlupgamesite.netlify.app",
 		githubLink: "https://github.com/MarianoFranco/gaming-project",
 	},
@@ -32,7 +32,7 @@ const projectsData = [
 		imageSrc: "/images/technoland/1.jpg",
 		smallDesc:
 			"A science museum website. This was my first semester project, in which I could consolidate my knowledge in Adobe xD, for desing, and using flexBox and CSS grids",
-		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+
 		projectLink: "https://thesciencemuseum.netlify.app",
 		githubLink: "",
 	},
@@ -52,7 +52,7 @@ const projectsData = [
 		imageSrc: "/images/nomada/1.jpg",
 		smallDesc:
 			"A travel blog. This was my first year project. The design was done with Figma and I create a dynamic web which is connected to an API with vanilla Javascript",
-		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+
 		projectLink: "https://nomadatravelblog.netlify.app",
 		githubLink:
 			"https://github.com/MarianoFranco/Project_exam_1_Mariano_Franco",
@@ -73,7 +73,7 @@ const projectsData = [
 		imageSrc: "/images/campshop/1.jpg",
 		smallDesc:
 			"An e-commerce website that has both customer-facing and admin sections. The project was populated by Strapi API and deployed in Heroku",
-		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+
 		projectLink: "https://campshop-sp.netlify.app",
 		githubLink: "https://github.com/MarianoFranco/campshop",
 	},
@@ -92,8 +92,7 @@ const projectsData = [
 		],
 		imageSrc: "/images/holidaze/holidaze-portrait.jpg",
 		smallDesc:
-			"My end of course project. I created a hotel booking, made with NextJs and connected to a Strapi Rest API",
-		desc: "<p >This was my first project of the course. To design the logo and the web use adobe xD. In which I learned to use the color palette and different techniques to apply the effects. As well as to differentiate the UI a bit from the UX.</p><p>In the code section, first use HTML and CSS. And as the year went by, I was able to apply the knowledge I gained from Javascript during the course.</p>",
+			"My final thesis report. I created a hotel booking, made with NextJs and connected to a Strapi Rest API",
 		projectLink: "https://holidaze-hotel-final-project.vercel.app/",
 		githubLink:
 			"https://github.com/MarianoFranco/holidaze-hotel-final-project",
